ba88f4ecac12fa5555eaf56040e87acf0053b456
SoporteGruca <soporte.sistemas@gruca.mx>
2026-09-18 10:55:45 -0600

    crear pagina login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './pages/login/login';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/login' }
+];
