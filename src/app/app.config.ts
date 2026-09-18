da30bc8865d82e427f6dfa4bcda1ff69198490e4
SoporteGruca <soporte.sistemas@gruca.mx>
2026-09-18 15:52:12 -0600

    fix:app config configuraciion inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideHttpClient(withFetch()),
     provideBrowserGlobalErrorListeners(),
-    provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration()
   ]
 };
